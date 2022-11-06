ab521bb9b37edf366f141382271bce1db407d708
unknown <fobas0405@gmail.com>
2022-11-06 21:03:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.icon');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    // hamburger.classList.toggle('show');
+    navMenu.classList.toggle('show');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('show');
+    navMenu.classList.remove('show');
+}))
